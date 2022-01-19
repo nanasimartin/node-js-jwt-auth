181c751b99f0df4a6997909adb6cc494500431a9
nanasimartin <nanasimartin5@gmail.com>
2022-01-19 12:29:58 +0100

    new

---
+++ app/routes/sajatbackend.js
@@ -0,0 +1,37 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  //sajatbackend vegpontok--------------------------------------------------------------
+  
+  app.get('/kerdesek', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from kerdesek', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+
+      res.send(rows)
+    })
+    
+    connection.end()    
+
+  })
+};
