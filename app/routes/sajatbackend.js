67ec7c5af05e7aaa6c5e181dd3b2b0542c446f31
nanasimartin <nanasimartin5@gmail.com>
2022-01-24 12:23:41 +0100

    jo

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -50,7 +50,7 @@ module.exports = function(app) {
     
     let dt=new Date();
     let teljesdat=dt.getFullYear()+"-"+  (dt.getMonth()+1)   +"-"+dt.getDate();
-    connection.query("INSERT INTO beerkezett VALUES (NULL, '"+req.body.beviteltomb[1]+"', '"+req.body.beviteltomb[2]+"','"+req.body.beviteltomb[3]+"','"+req.body.beviteltomb[4]+"','"+req.body.beviteltomb[5]+"','"+req.body.beviteltomb[6]+"','"+req.body.beviteltomb[7]+"','"+req.body.beviteltomb[8]+"','"+req.body.beviteltomb[9]+"','"+req.body.beviteltomb[10]+"','"+req.body.bevitel1+"', '"+teljesdat+"') ", function (err, rows, fields) {
+    connection.query("INSERT INTO beerkezett VALUES (NULL, '"+req.body.beviteltomb[1]+"', '"+req.body.beviteltomb[2]+"','"+req.body.beviteltomb[3]+"','"+req.body.beviteltomb[4]+"','"+req.body.beviteltomb[5]+"','"+req.body.beviteltomb[6]+"','"+req.body.beviteltomb[7]+"','"+req.body.beviteltomb[8]+"','"+req.body.beviteltomb[9]+"','"+req.body.beviteltomb[10]+"','"+req.body.beviteltomb[11]+"''"+req.body.bevitel1+"', '"+teljesdat+"') ", function (err, rows, fields) {
       if (err) throw err
     
       console.log("Sikeres kitoltes!")
@@ -61,6 +61,29 @@ module.exports = function(app) {
     connection.end()    
 
   })  
+
+  app.post('/torles', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    connection.query('DELETE  from kerdesek where kerdesek_id=' + req.body.bevitel1, function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+
+      res.send(rows)
+    })
+    
+    connection.end()    
+
+  })
 
 };
 
