e000b59c547adb1ae2cdcb3b043f20d147cddb6f
nanasimartin <nanasimartin5@gmail.com>
2022-01-25 12:25:22 +0100

    uj

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -62,6 +62,77 @@ module.exports = function(app) {
 
   })  
 
+  app.get('/tema', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from uzenet ORDER BY uzenet_id DESC ', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+  
+      res.send(rows)
+    })
+    
+    connection.end()    
+  
+  })
+
+  app.post('/kommentfelv', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    let dt=new Date();
+    let teljesdat=dt.getFullYear()+"-"+  (dt.getMonth()+1)   +"-"+dt.getDate();
+    connection.query("INSERT INTO uzenet VALUES (NULL, '"+req.body.bevitel1+"', '"+req.body.bevitel2+"', '"+teljesdat+"') ", function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log("Sikeres felvitel!")
+
+      res.send("Sikeres felvitel!")
+    })
+    
+    connection.end()    
+
+  })
+
+  app.post('/uzenettorles', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    connection.query('DELETE  from uzenet where uzenet_id=' + req.body.bevitel1, function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log("Sikeres Törlés!")
+
+      res.send("Sikeres Törlés!")
+    })
+    
+    connection.end()    
+
+  })
+
   app.post('/torles', (req, res) => {
     var mysql = require('mysql')
     var connection = mysql.createConnection({
@@ -76,14 +147,18 @@ module.exports = function(app) {
     connection.query('DELETE  from kerdesek where kerdesek_id=' + req.body.bevitel1, function (err, rows, fields) {
       if (err) throw err
     
-      console.log(rows)
+      console.log("Sikeres Törlés!")
 
-      res.send(rows)
+      res.send("Sikeres Törlés!")
     })
     
     connection.end()    
 
   })
 
+  
+
 };
+
+
 
